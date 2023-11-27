a55a3d9c911e049f3bc7d440c0969c59ada47981
DiligentBuilder <71938320+DiligentBuilder@users.noreply.github.com>
2023-11-27 12:45:10 -0500

    Add updated BopItCode via upload

--- ArduinoBopItCode.ino
+++ ArduinoBopItCode.ino
@@ -1,8 +1,19 @@
- // setup buttons
+#include <Wire.h>
+#include <Adafruit_MMA8451.h>
+#include <Adafruit_Sensor.h>
+
+#include <LiquidCrystal_I2c.h>
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+Adafruit_MMA8451 mma = Adafruit_MMA8451();
+
+// setup buttons
   const int button1Pin = 7;
   const int button2Pin = 6;
-  const int button3Pin = 5;
-  const int button4Pin = 6;
+  //const int button3Pin = 5;
+  //const int button4Pin = 6;
+  const int SlideSwitchPin = A0;
   const int ledPin = 4;
 
 // setting up score variable
@@ -21,25 +32,79 @@ void setup() {
   // start the serial input for debugging purposes
   Serial.begin(9600);
 
+  // accelerometer setup code
+
+  Serial.println("Adafruit MMA8451 test!");
+  
+
+  if (! mma.begin()) {
+    Serial.println("Couldnt start");
+    while (1);
+  }
+  Serial.println("MMA8451 found!");
+  
+  mma.setRange(MMA8451_RANGE_2_G);
+  
+  Serial.print("Range = "); Serial.print(2 << mma.getRange());  
+  Serial.println("G");
+
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
+  
+
   // Display welcome message
   Serial.println("Welcome to Bop-It!");
 
 
  
-  
+  // setup the pins for the user inputs
   pinMode(button1Pin, INPUT);
   pinMode(button2Pin, INPUT);
-  pinMode(button3Pin, INPUT);
-  pinMode(button4Pin, INPUT);
+  //pinMode(button3Pin, INPUT);
+  //pinMode(button4Pin, INPUT);
+
+  
+  pinMode(SlideSwitchPin, INPUT);
   
   // setup the LED
   pinMode(ledPin, OUTPUT);
+
+
+  // setup the LCD display
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
+
   
 
   
 
 
   // pick a random song
+
+  // there are three songs, randomly choose a song to play out of the three possible songs
+
+  // generate random number between 1 and 3
+  int randNumberforSongSelection = random(1, 4);
+
+  if (randNumberforSongSelection == 1) {
+
+    // start playing the first song
+    
+  }
+
+  if (randNumberforSongSelection == 2) {
+
+    // start playing the second song
+
+  }
+
+  if (randNumberforSongSelection == 3) {
+
+    // start playing the third song
+
+  }
 
 
   
@@ -61,14 +126,24 @@ void loop() {
 
   if (startState == HIGH) {
     Serial.println("Game starting in 3...");
+    lcd.println("Game starting in 3...");
     delay(1000);
 
-    Serial.println("2...");
+    lcd.clear();
+
+    Serial.println("Game starting in 2...");
+    lcd.println("Gane starting in 2...");
     delay(1000);
 
 
-    Serial.println("1..");
+    lcd.clear();
+
+
+    Serial.println("Game starting in 1...");
+    lcd.println("Game starting in 1...");
     delay(1000);
+
+    lcd.clear();
 
 
     // enter the game loop
@@ -101,12 +176,15 @@ void loop() {
       // printing command to user, telling user what input they must provide
       if (randNumber == 1) {
         Serial.println("Push it!");
+        lcd.println("Push it!");
       }
       else if (randNumber == 2) {
         Serial.println("Slide it!");
+        lcd.println("Slide it!");
       }
       else if (randNumber == 3) {
         Serial.println("Spin it!");
+        lcd.println("Spin it!");
       }
 
       // decreasing the time interval of waiting
@@ -129,8 +207,18 @@ void loop() {
       int spunState = 0;
       int slidState = 0;
       pushedButtonState = digitalRead(button2Pin);
-      spunState = digitalRead(button3Pin);
+      //spunState = digitalRead(button3Pin);
       slidState = digitalRead(button4Pin);
+
+      // for the spinning, detect if the X, Y, or Z acceleration exceeds the threshold, if so, a spun has been detected
+      mma.read();
+      sensors_event_t event;
+      mma.getEvent(&event);
+
+      if (event.acceleration.x > 1 || event.acceleration.y > 1 || event.acceleration.z > 1) {
+        // a spin has been detected if the X, Y, or Z acceleration exceeds the threshold
+        spunState = HIGH;
+      }
 
       
 
@@ -159,6 +247,11 @@ void loop() {
           Serial.println("Incorrect input!");
           Serial.println("Game over!");
 
+          lcd.println("Incorrect input!");
+          delay(5000);
+          lcd.clear();
+          lcd.println("Game over!");
+
           gameLoop = false;
           break;
         }
@@ -169,6 +262,11 @@ void loop() {
           Serial.println("Incorrect input!");
           Serial.print("Game over!");
 
+          lcd.println("Incorrect input!");
+          delay(5000);
+          lcd.clear();
+          lcd.print("Game over!");
+
           gameLoop = false;
           break;
         }
@@ -177,7 +275,14 @@ void loop() {
       if (randNumber == 3) {
         if (pushed == true || spun == true || slid == false) {
           Serial.println("Incorrect input!");
+          delay(5000);
+          lcd.clear();
           Serial.println("Game over!");
+
+          lcd.println("Incorrect input!");
+          delay(5000);
+          lcd.clear();
+          lcd.println("Game over!");
           
           gameLoop = false;
           break;
@@ -190,6 +295,12 @@ void loop() {
       if (score == 99) {
         Serial.println("Congratulations, you have a score of 99 points, and you have successfully reached the end of the game!");
         Serial.println("Game over!");
+
+        lcd.println("Congratulations, you have a score of 99 points, and you have successfully reached the end of the game!");
+        delay(5000);
+        lcd.clear();
+
+        lcd.println("Game over!");
 
         // once the game is over, exit the game loop and return back to the before the game state
         gameLoop = false;
