fa18f779f44477cb1a464738ff26d106dd417f7d
DiligentBuilder <71938320+DiligentBuilder@users.noreply.github.com>
2023-12-01 17:40:38 -0500

    Playing tones should be working correctly

--- ArduinoBopItCode.ino
+++ ArduinoBopItCode.ino
@@ -1,3 +1,8 @@
+// defining the notes for playing the sounds that play when the commands are issued
+#define NOTE_C4  262
+#define NOTE_E4  330
+#define NOTE_G4  392
+
 #include <SD.h> // need to include the SD library
 //#define SD_ChipSelectPin 4 //connect pin 4 of arduino to cs pin of sd card
 //#include <TMRpcm.h> //Arduino library for asynchronous playback of PCM/WAV files
